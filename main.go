bf690b19f4d08b82b53a7f84afc5ec059de05e7a
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2021-02-28 21:21:05 +0600

    First commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+      
+      var student [5] string
+      student[0]="tahsin"
+      student[1]="tithi"
+      student[2]="tareq"
+      student[3]="anonna"
+      student[4]="ferdaus"      
+      
+      x:=make ([]string, 0)
+
+      fmt.Println(x)
+    
+
+
+
+}
